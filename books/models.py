b887d1d056f0727e327c761c55944e8f41352a95
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-17 13:37:25 -0400

    book model added

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.fields import DateTimeField
 
 
 class User(AbstractUser):
@@ -8,3 +9,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000)
+    url = models.URLField(max_length=250)
+    created_at = DateTimeField(auto_now_add = True)
+  
+    
+    def __str__(self):
+        return f"{self.title}, {self.author}, {self.description}"
