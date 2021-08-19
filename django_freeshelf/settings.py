28267b4a4417583681401f33fc9c4aaa1e22dc2e
sarah-wines <sarah.lodico.wines@gmail.com>
2021-08-18 20:36:45 -0400

    connected css

--- django_freeshelf/settings.py
+++ django_freeshelf/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env()
 DEBUG = env("DEBUG")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static/"),
+    os.path.join(BASE_DIR, "static"),
 )
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
